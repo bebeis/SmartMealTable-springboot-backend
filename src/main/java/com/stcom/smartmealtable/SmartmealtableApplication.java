617c8953f99540453eabbd2d4bb43c4d91b3ead3
Junseo Bae <ferrater1013@gmail.com>
2025-04-24 16:17:40 +0900

    init: 프로젝트 초기 생성

---
+++ src/main/java/com/stcom/smartmealtable/SmartmealtableApplication.java
@@ -0,0 +1,13 @@
+package com.stcom.smartmealtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartmealtableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartmealtableApplication.class, args);
+	}
+
+}
