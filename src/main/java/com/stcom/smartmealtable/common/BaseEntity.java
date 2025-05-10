07650b8312e85b713d6daa55dc5c6a28498cf6fa
Junseo Bae <ferrater1013@gmail.com>
2025-05-10 23:30:13 +0900

    refactor: BaseEntity 클래스 상속 관계로 분리

--- src/main/java/com/stcom/smartmealtable/common/BaseEntity.java
+++ src/main/java/com/stcom/smartmealtable/common/BaseEntity.java
@@ -2,21 +2,19 @@ package com.stcom.smartmealtable.common;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseEntity {
+public class BaseEntity extends BaseTimeEntity {
 
-    @CreatedDate
-    private LocalDateTime createdDateTime;
+    @CreatedBy
+    private String createdBy;
 
-    @LastModifiedDate
-    private LocalDateTime modifiedDateTime;
-
+    @LastModifiedBy
+    private String lastModifiedBy;
 }
