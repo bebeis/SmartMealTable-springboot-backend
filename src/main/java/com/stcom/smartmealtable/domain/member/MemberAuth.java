54b244294e0c6a9c9f8efefb29713c0309896c25
Junseo Bae <ferrater1013@gmail.com>
2025-05-11 01:28:20 +0900

    feat(MemberAuth): 도메인 기능 추가

--- src/main/java/com/stcom/smartmealtable/domain/member/MemberAuth.java
+++ src/main/java/com/stcom/smartmealtable/domain/member/MemberAuth.java
@@ -1,16 +1,21 @@
 package com.stcom.smartmealtable.domain.member;
 
-import com.stcom.smartmealtable.common.BaseEntity;
+import com.stcom.smartmealtable.common.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.Email;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
-public class MemberAuth extends BaseEntity {
+@NoArgsConstructor
+public class MemberAuth extends BaseTimeEntity {
 
     @Id
     @GeneratedValue
@@ -23,10 +28,23 @@ public class MemberAuth extends BaseEntity {
     @Embedded
     private MemberPassword password;
 
-    private boolean isEmailVerified;
+    // TODO: 이메일 인증 기능 구현해야함
+    private boolean isEmailVerified = true;
 
-    @OneToOne(mappedBy = "memberAuth")
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_profile_id")
     private MemberProfile memberProfile;
+
+    @Builder
+    public MemberAuth(String email, String rawPassword) throws PasswordPolicyException {
+        this.email = email;
+        this.password = new MemberPassword(rawPassword);
+    }
+
+    public void registerMemberProfile(MemberProfile profile) {
+        memberProfile = profile;
+        profile.linkMemberAuth(this);
+    }
 
     public void changePassword(String rawOldPassword, String rawNewPassword)
             throws PasswordFailedExceededException, PasswordPolicyException {
@@ -43,4 +61,10 @@ public class MemberAuth extends BaseEntity {
     public boolean isEmailVerified() {
         return isEmailVerified;
     }
+
+    public void verifyEmail() {
+        this.isEmailVerified = true;
+    }
+
+
 }
