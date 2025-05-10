e83267e8b741f9bca225207fa8681651093ba50e
Junseo Bae <ferrater1013@gmail.com>
2025-05-11 01:26:12 +0900

    feat(Address): address 도메인 추가

---
+++ src/main/java/com/stcom/smartmealtable/domain/common/Address.java
@@ -0,0 +1,46 @@
+package com.stcom.smartmealtable.domain.common;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String lotNumberAddress;
+
+    private String roadAddress;
+
+    private String detailAddress;
+
+    private String alias;
+
+    private BigDecimal latitude;
+
+    private BigDecimal longitude;
+
+    @Enumerated(EnumType.STRING)
+    private AddressType type;
+
+    public void updateAddress(String lotNumberAddress, String roadAddress, String detailAddress,
+                              BigDecimal latitude, BigDecimal longitude) {
+        this.lotNumberAddress = lotNumberAddress;
+        this.roadAddress = roadAddress;
+        this.detailAddress = detailAddress;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public void changeAddressType(AddressType newType) {
+        this.type = newType;
+    }
+}
---
+++ src/main/java/com/stcom/smartmealtable/domain/common/AddressType.java
@@ -0,0 +1,5 @@
+package com.stcom.smartmealtable.domain.common;
+
+public enum AddressType {
+    
+}
