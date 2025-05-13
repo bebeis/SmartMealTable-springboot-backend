28435d9d259688eaea7c0bae738193192d285d80
Junseo Bae <ferrater1013@gmail.com>
2025-05-13 19:43:28 +0900

    feat(food): food 엔티티 추가

---
+++ src/main/java/com/stcom/smartmealtable/domain/food/FoodCategory.java
@@ -0,0 +1,5 @@
+package com.stcom.smartmealtable.domain.food;
+
+public enum FoodCategory {
+    KOREAN, JAPANESE, WESTERN, CHINESE
+}
---
+++ src/main/java/com/stcom/smartmealtable/domain/food/FoodPreference.java
@@ -0,0 +1,54 @@
+package com.stcom.smartmealtable.domain.food;
+
+import com.stcom.smartmealtable.common.BaseTimeEntity;
+import com.stcom.smartmealtable.domain.member.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class FoodPreference extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "food_preference_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category")
+    private FoodCategory category;
+
+    private boolean isPreferred;
+
+    private Double weight;
+
+    @Builder
+    public FoodPreference(Member member, FoodCategory category, boolean isPreferred, Double weight) {
+        this.member = member;
+        this.category = category;
+        this.isPreferred = isPreferred;
+        this.weight = weight;
+    }
+
+
+    public void updatePreference(boolean isPreferred, Double weight) {
+        this.isPreferred = isPreferred;
+        this.weight = weight;
+    }
+
+}
