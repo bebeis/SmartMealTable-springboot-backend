108d3a21c61ad7fa032ef16eefb31f089df77863
Junseo Bae <ferrater1013@gmail.com>
2025-05-13 19:45:58 +0900

    feat: 이메일로 엔티티 조회하는 메서드 추가

--- src/main/java/com/stcom/smartmealtable/domain/member/MemberRepository.java
+++ src/main/java/com/stcom/smartmealtable/domain/member/MemberRepository.java
@@ -1,7 +1,10 @@
 package com.stcom.smartmealtable.domain.member;
 
+import jakarta.validation.constraints.Email;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    List<Member> findMemberByEmail(@Email String email);
 }
