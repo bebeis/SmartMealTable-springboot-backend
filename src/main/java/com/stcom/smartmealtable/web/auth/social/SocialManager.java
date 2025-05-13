ed5432fe51778b514860f3895a168659b8bd280f
Junseo Bae <ferrater1013@gmail.com>
2025-05-13 22:22:18 +0900

    fix: 스프링 빈을 다시 생성하는 오류 수정

--- src/main/java/com/stcom/smartmealtable/web/auth/social/SocialManager.java
+++ src/main/java/com/stcom/smartmealtable/web/auth/social/SocialManager.java
@@ -1,21 +1,23 @@
 package com.stcom.smartmealtable.web.auth.social;
 
+import static com.stcom.smartmealtable.web.auth.social.SocialConst.KAKAO;
+
 import com.stcom.smartmealtable.web.dto.token.TokenDto;
+import jakarta.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClient.RequestBodySpec;
 import org.springframework.web.client.RestClient.ResponseSpec;
 
 @Component
+@RequiredArgsConstructor
 public class SocialManager {
 
     private final Map<String, SocialHttpMessage> socialMap = new HashMap<>();
-
-    public SocialManager() {
-        socialMap.put("Kakao", new KakaoHttpMessage());
-    }
+    private final KakaoHttpMessage kakaoHttpMessage;
 
     public RequestBodySpec getTokenRequestMessage(RestClient client, String provider, String code) {
         return socialMap.get(provider).getRequestMessage(client, code);
@@ -23,5 +25,10 @@ public class SocialManager {
 
     public TokenDto getTokenResponse(ResponseSpec responseSpec, String provider) {
         return socialMap.get(provider).getTokenResponse(responseSpec);
+    }
+
+    @PostConstruct
+    public void init() {
+        socialMap.put(KAKAO, kakaoHttpMessage);
     }
 }
