1ac7e46a000a577dc16703e655b6f612d2d12d0b
Junseo Bae <ferrater1013@gmail.com>
2025-05-13 19:49:00 +0900

    feat(Auth): 인증 api 엔드포인트 구현

---
+++ src/main/java/com/stcom/smartmealtable/web/auth/OAuth2Controller.java
@@ -0,0 +1,86 @@
+package com.stcom.smartmealtable.web.auth;
+
+import com.stcom.smartmealtable.domain.social.SocialAccountService;
+import com.stcom.smartmealtable.security.JwtTokenService;
+import com.stcom.smartmealtable.web.auth.social.SocialManager;
+import com.stcom.smartmealtable.web.dto.ApiResponse;
+import com.stcom.smartmealtable.web.dto.token.JwtTokenResponseDto;
+import com.stcom.smartmealtable.web.dto.token.TokenDto;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClient.RequestBodySpec;
+
+@RestController
+@Slf4j
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class OAuth2Controller {
+
+    private final SocialManager socialManager;
+    private final RestClient client = RestClient.create();
+    private final SocialAccountService socialAccountService;
+    private final JwtTokenService jwtTokenService;
+
+    @PostMapping("/oauth2/code")
+    public ApiResponse<JwtTokenResponseDto> getTokenFromSocial(@RequestBody JwtTokenRequest request) {
+        RequestBodySpec tokenRequestMessage = socialManager.getTokenRequestMessage(client, request.getProvider(),
+                request.getAuthorizationCode());
+        TokenDto token = socialManager.getTokenResponse(tokenRequestMessage.retrieve(), request.getProvider());
+        boolean isNewUser = socialAccountService.isNewUser(token.getProvider(),
+                token.getProviderUserId());
+        if (!isNewUser) {
+            socialAccountService.createNewAccount(token);
+        }
+
+        JwtTokenResponseDto tokenDto = jwtTokenService.createTokenDto(
+                socialAccountService.findMemberId(token.getProvider(), token.getProviderUserId()));
+        tokenDto.setNewUser(isNewUser);
+        return ApiResponse.createSuccess(tokenDto);
+    }
+
+    @PostMapping("/api/v1/auth/token/refresh")
+    public ApiResponse<JwtRefreshedAccessTokenDto> refreshAccessToken(@RequestBody JwtRefreshTokenRequest request) {
+        String memberId = jwtTokenService.extractMemberIdFromRefreshToken(request.getRefreshToken());
+        String accessToken = jwtTokenService.createAccessToken(memberId);
+
+        return ApiResponse.createSuccess(
+                new JwtRefreshedAccessTokenDto(accessToken, 3600, "Bearar")
+        );
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    static class JwtTokenRequest {
+
+        @NotEmpty
+        private String provider;
+
+        @NotEmpty
+        private String authorizationCode;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class JwtRefreshedAccessTokenDto {
+        private String accessToken;
+        private int expiresIn;
+        private String tokenType;
+    }
+
+    @Data
+    static class JwtRefreshTokenRequest {
+
+        @NotEmpty
+        private String refreshToken;
+    }
+
+}
