282dabc3c1e138b9b2ec37ec38e8854735b3b99e
Junseo Bae <ferrater1013@gmail.com>
2025-06-09 17:02:24 +0900

    fix: Group Api 응답 데이터 수정
    ID 값을 포함하여 반환화도록 합니다.

--- src/main/java/com/stcom/smartmealtable/web/controller/GroupController.java
+++ src/main/java/com/stcom/smartmealtable/web/controller/GroupController.java
@@ -33,11 +33,13 @@ public class GroupController {
     @Data
     @AllArgsConstructor
     static class GroupDto {
+        private Long id;
         private String roadAddress;
         private String name;
         private String groupType;
 
         public GroupDto(Group group) {
+            this.id = group.getId();
             this.groupType = group.getTypeName();
             this.name = group.getName();
             this.roadAddress = group.getAddress().getRoadAddress();
